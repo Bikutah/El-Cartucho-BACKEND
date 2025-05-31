5d8a4946d35f240c79d9333854867ed903975983
Victor Hugo Cretton Rivera <victorcrettonrivera@gmail.com>
2025-05-31 18:56:20 -0300

    Prueba de api

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login'])->name('login.submit');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::get('/api/producto/listar', [ProductoController::class, 'buscar']);
+Route::get('/prueba/producto/listar', [ProductoController::class, 'buscar']);
 
 // Rutas protegidas
 Route::middleware(['auth', AdminMiddleware::class])->group(function () {
