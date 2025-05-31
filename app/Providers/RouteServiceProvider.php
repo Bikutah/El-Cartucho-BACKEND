5af032d1588ab9d4f7ee75dc986b3e9e94923176
Victor Hugo Cretton Rivera <victorcrettonrivera@gmail.com>
2025-05-31 19:33:46 -0300

    Pruebas de api 7

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -23,7 +23,6 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
-                ->prefix('ed')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
